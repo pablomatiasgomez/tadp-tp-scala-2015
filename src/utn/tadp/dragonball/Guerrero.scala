1320d2483d04fa87447985683463c8324d05df91
GastonTS <gastonsantoalla@gmail.com>
2015-11-02 11:25:02 -0300

    Delete useless line

--- src/utn/tadp/dragonball/Guerrero.scala
+++ src/utn/tadp/dragonball/Guerrero.scala
@@ -17,12 +17,11 @@ object Simulador {
   def dejarseFajar(combatientes : (Guerrero, Guerrero)) = combatientes
   
   def cargarKi(combatientes : (Guerrero, Guerrero)) = {
-    val (guerrero,oponente) = (combatientes._1,combatientes._2)
-     (combatientes._1.especie, combatientes._2) match {
-      case (Saiyajing(nivel), _) if nivel > 0 => (combatientes._1.aumentarKi(150* nivel), combatientes._2) 
-      case (Androide, _) => (combatientes._1, combatientes._2)
-      case (_, _) => (combatientes._1.aumentarKi(100), combatientes._2)
+    (combatientes._1.especie, combatientes._2) match {
+        case (Saiyajing(nivel), _) if nivel > 0 => (combatientes._1.aumentarKi(150* nivel), combatientes._2) 
+        case (Androide, _) => (combatientes._1, combatientes._2)
+        case (_, _) => (combatientes._1.aumentarKi(100), combatientes._2)
     }
   }
-  
+ 
 }
