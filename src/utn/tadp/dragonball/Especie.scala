0a1a678ba9890f9d2c8642a36ced2215ea8e48b0
j9794 <juanmanuel9794@hotmail.com>
2015-11-02 12:12:09 -0300

    Juan is back.

--- src/utn/tadp/dragonball/Especie.scala
+++ src/utn/tadp/dragonball/Especie.scala
@@ -1,13 +1,14 @@
 package utn.tadp.dragonball
 
-abstract class Especie(){
+abstract class Especie(val fusionable:Boolean=false){
   
 }
 
-case object Humano extends Especie
+case object Humano extends Especie(true)
 case class Saiyajing(
   nivel : Int  
-  ) extends Especie
+  ) extends Especie(true)
 case object Androide extends Especie
-case object Namekusein extends Especie
+case object Namekusein extends Especie(true)
 case object Monstruo extends Especie
+case class  Fusion(miembros:(Guerrero,Guerrero)) extends Especie
--- src/utn/tadp/dragonball/Guerrero.scala
+++ src/utn/tadp/dragonball/Guerrero.scala
@@ -6,17 +6,18 @@ case class Guerrero(
       energia : Int,
       especie : Especie
       ) {
-  
   def aumentarKi(ki: Int) = 
       copy(energia = energia + ki)
-
+  def puedeFusionarse = especie.fusionable
  }
 
 object Simulador {
  
-  def dejarseFajar(combatientes : (Guerrero, Guerrero)) = combatientes
+  type Movimiento = (Guerrero,Guerrero)
   
-  def cargarKi(combatientes : (Guerrero, Guerrero)) = {
+  def dejarseFajar(combatientes : (Guerrero, Guerrero)):Movimiento = combatientes
+  
+  def cargarKi(combatientes : (Guerrero, Guerrero)):Movimiento = {
     (combatientes._1.especie, combatientes._2) match {
         case (Saiyajing(nivel), _) if nivel > 0 => (combatientes._1.aumentarKi(150* nivel), combatientes._2) 
         case (Androide, _) => (combatientes._1, combatientes._2)
@@ -24,4 +25,13 @@ object Simulador {
     }
   }
  
-}
+  def fusion(aliado:Guerrero,combatiente:Guerrero,oponente:Guerrero):Movimiento = {
+    if(List(aliado,combatiente).forall(_.puedeFusionarse))
+       (combatiente.copy(  inventario=combatiente.inventario++aliado.inventario,
+                           energia=combatiente.energia + aliado.energia,
+                           especie=Fusion(combatiente,aliado)),
+        oponente)
+    else throw new RuntimeException("No se pueden fusionar")
+    }
+  }
+  
