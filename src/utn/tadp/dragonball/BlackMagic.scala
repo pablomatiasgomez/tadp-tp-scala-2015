db6590a3984c9842582c1ab5158bf2cc9e6c0d55
j9794 <juanmanuel9794@hotmail.com>
2015-11-04 02:02:31 -0300

    Tuplas de orden superior.

--- src/utn/tadp/dragonball/BlackMagic.scala
+++ src/utn/tadp/dragonball/BlackMagic.scala
@@ -7,4 +7,9 @@ object BlackMagic {
     def count(n: T, q:Int):Boolean = list.count(n) == q
   }
    
+   implicit class SuperiorTuple2[T,U](tupla: Tuple2[T,U]) {
+    def on_1[S](f:(T=>S)):Tuple2[S,U] = (f(tupla._1), tupla._2)
+    def on_2[S](g:(U=>S)):Tuple2[T,S] = (tupla._1, g(tupla._2))
+  }
+   
 }
