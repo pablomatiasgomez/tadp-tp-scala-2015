f1d7769b22f03ed63aa9070d6275514880cfbaa2
GastonTS <gastonsantoalla@gmail.com>
2015-11-05 18:47:15 -0300

    Test Pelear un Round

--- test/utn/tadp/dragonball/Guerrero_Test.scala
+++ test/utn/tadp/dragonball/Guerrero_Test.scala
@@ -6,13 +6,12 @@ import org.scalatest._
 
 class Guerrero_Test extends FlatSpec with Matchers {
 
-  def deberia = should
   val todosSaben: List[Movimiento] = List(DejarseFajar, CargarKi, MuchosGolpesNinja, Onda(10))
   val goku : Guerrero = new Guerrero("Goku", List(SemillaDelErmitaño, FotoDeLaLuna), 2500, 1300, Saiyajin(SuperSaiyajin(1, 500), true), Fajado(3), todosSaben++List(Onda(99), Genkidama))
   val vegeta : Guerrero = new Guerrero("Vegeta", List(), 1001, 801, Saiyajin(Normal, false), Luchando, todosSaben++List(Onda(100), Fusion(goku)))
 
   def quedoConMasKi(combatientes: Combatientes) = combatientes._1.energia
-  def mayorVentajaDeKi(combatientes: Combatientes) = (combatientes._2. energia - combatientes._1.energia).abs
+  def mayorVentajaDeKi(combatientes: Combatientes) = combatientes._2. energia - combatientes._1.energia
   def quedoMasFajado(combatientes: Combatientes) = combatientes._1.estado match {
                                                                                 case Fajado(rounds) => rounds
                                                                                 case _ => 0
@@ -20,20 +19,33 @@ class Guerrero_Test extends FlatSpec with Matchers {
   
   
   "goku" should "elegir CargarKi porque lo deja con mas ki" in {
-    goku.movimientoMasEfectivoContra(vegeta)(quedoConMasKi) should be (CargarKi)
+    goku.movimientoMasEfectivoContra(vegeta)(quedoConMasKi) shouldBe CargarKi
   }
   
   "goku" should "elegir Genkidama porque lo deja con mayor ventaja" in {
-    goku.movimientoMasEfectivoContra(vegeta)(mayorVentajaDeKi) should be (Genkidama)
+    goku.movimientoMasEfectivoContra(vegeta)(mayorVentajaDeKi) shouldBe Genkidama
   }
   
   "vegeta" should "elegir Fusion porque lo deja con mas ki y con mas Venjata" in {
-    vegeta.movimientoMasEfectivoContra(goku)(quedoConMasKi) should be (Fusion(goku))
-    vegeta.movimientoMasEfectivoContra(goku)(mayorVentajaDeKi) should be (Fusion(goku))
+    vegeta.movimientoMasEfectivoContra(goku)(quedoConMasKi) shouldBe Fusion(goku)
+    vegeta.movimientoMasEfectivoContra(goku)(mayorVentajaDeKi) shouldBe Fusion(goku)
   }
   
   "goku" should "elegir DejarseFajar porque lo deja mas fajado" in {
-    goku.movimientoMasEfectivoContra(vegeta)(quedoMasFajado) should be (DejarseFajar)
+    goku.movimientoMasEfectivoContra(vegeta)(quedoMasFajado) shouldBe DejarseFajar
+  }
+  
+  "goku" should "cargar ki mientras que vegeta se fusiona como contra ataque" in {
+    val movimientoDeGoku = goku.movimientoMasEfectivoContra(vegeta)(quedoConMasKi)
+
+    val (g, v) = goku.pelearUnRound(movimientoDeGoku)(vegeta)
+    
+    movimientoDeGoku shouldBe CargarKi
+    g.estado shouldBe Luchando
+    g.energia shouldBe 1450
+    v.energia shouldBe 2101
+    v.energiaMaxima shouldBe 3501
+    v.especie shouldBe Fusionado(goku, vegeta)
   }
   
 }
