52ded8142dd23845cb76a7f3d7fed98b48207f3c
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2015-11-26 21:03:44 -0300

    format

--- src/utn/tadp/dragonball/Simulador.scala
+++ src/utn/tadp/dragonball/Simulador.scala
@@ -16,18 +16,14 @@ object Simulador {
         case (Inconsciente, _) => combatientes
         case (_, DejarseFajar) => movimiento(combatientes)
         case (_, _) => movimiento(combatientes) onFst (_ resetearTurnosFajados)
-
       }
     }
-    
   }
   
   abstract class AutoMovimiento extends Movimiento {
     def autoMovimiento: Guerrero=>Guerrero
     def movimiento = ( _ onFst autoMovimiento)
   }
-  
-
   
   case object DejarseFajar extends AutoMovimiento {
     def autoMovimiento = _ pasarTurnoFajado    
@@ -46,44 +42,41 @@ object Simulador {
   case class UsarItem(item: Item) extends Movimiento {
     def movimiento = combatientes => {
       
-    def disparado: Especie => Guerrero => Guerrero = ({ 
-      case Humano => _ disminuiEnergia 20
-      case Namekusein => _.transformOnTrue( _.estado == Inconsciente)(_ disminuiEnergia 10)
-      case _ => identity
-    })
-    
-    def perderCola(guerrero: Guerrero): Guerrero = guerrero.estado match{
-      case MonoGigante(energiaNormal) => (guerrero tuEnergiaMaximaEs energiaNormal
-                                                   estas Inconsciente)
-      case fase => guerrero
+      def disparado: Especie => Guerrero => Guerrero = ({ 
+        case Humano => _ disminuiEnergia 20
+        case Namekusein => _.transformOnTrue( _.estado == Inconsciente)(_ disminuiEnergia 10)
+        case _ => identity
+      })
+      
+      def perderCola(guerrero: Guerrero): Guerrero = guerrero.estado match{
+        case MonoGigante(energiaNormal) => (guerrero tuEnergiaMaximaEs energiaNormal
+                                                     estas Inconsciente)
+        case fase => guerrero
+      }
+      
+      val(atacante, oponente) = combatientes
+      combatientes.becomeOnTrue(atacante tiene item)(
+        (item, oponente.especie) match {
+          case (Arma(Roma), Androide) => combatientes
+          case (Arma(Roma), _) if oponente.energia < 300 => (atacante, oponente estas Inconsciente)
+          case (Arma(Filosa), Saiyajin(true)) => (atacante, perderCola(oponente transformateEn Saiyajin(false) tuEnergiaEs 1))
+          case (Arma(Filosa), _) => (atacante, oponente disminuiEnergia (atacante.energia / 100))
+          case (Arma(Fuego(tipo)), especieAtacado) if atacante tiene Municion(tipo) =>
+            (atacante gastarItems (List(Municion(tipo))), disparado(especieAtacado)(oponente))
+          case (SemillaDelErmitaño, _) => (atacante tuEnergiaEs (atacante.energiaMaxima), oponente)
+          case _ => combatientes
+        })
     }
-    
-    val(atacante, oponente) = combatientes
-    combatientes.becomeOnTrue(atacante tiene item)(
-      (item, oponente.especie) match {
-        case (Arma(Roma), Androide) => combatientes
-        case (Arma(Roma), _) if oponente.energia < 300 => (atacante, oponente estas Inconsciente)
-        case (Arma(Filosa), Saiyajin(true)) => (atacante, perderCola(oponente transformateEn Saiyajin(false) tuEnergiaEs 1))
-        case (Arma(Filosa), _) => (atacante, oponente disminuiEnergia (atacante.energia / 100))
-        case (Arma(Fuego(tipo)), especieAtacado) if atacante tiene Municion(tipo) =>
-          (atacante gastarItems (List(Municion(tipo))), disparado(especieAtacado)(oponente))
-        case (SemillaDelErmitaño, _) => (atacante tuEnergiaEs (atacante.energiaMaxima), oponente)
-        case _ => combatientes
-        })   
-    
-     }
   }
   
-
   case object ComerseAlOponente extends Movimiento { 
     def movimiento = combatientes => {
-    
       val(atacante, oponente) = combatientes
+
       atacante.especie match {
         case Monstruo(digerir) => (digerir(combatientes), oponente estas Muerto)
         case _ => combatientes
-    }
-    
+      }
     }
   }
   
@@ -99,13 +92,11 @@ object Simulador {
                                                               cargarAlMaximo)
         case _ => guerrero
       }
-      
     }
   }
   
   case object ConvertirseEnSuperSaiyajing extends AutoMovimiento {
     def autoMovimiento = guerrero => {
-      
       (guerrero.especie, guerrero.estado, guerrero.energia, guerrero.energiaMaxima) match {
         case (Saiyajin(_), MonoGigante(_), _, _) => guerrero
         case (Saiyajin(_), fase: EstadoSaiyajing, ki, kiMaximo) if (ki > kiMaximo/2) => 
@@ -117,37 +108,35 @@ object Simulador {
                                       (guerrero estas SuperSaiyajin(nivel, energiaOriginal)
                                                 tuEnergiaMaximaEs (5 * nivel * energiaOriginal))
         case _ => guerrero
-        }
-      
+      }
     }
   }
   
-  case class Fusion(aliado: Guerrero) extends AutoMovimiento  {
+  case class Fusion(aliado: Guerrero) extends AutoMovimiento {
     def autoMovimiento = guerrero => {
-        
-      (guerrero.especie,aliado.especie) match{
+      (guerrero.especie,aliado.especie) match {
         case (_:Fusionable, _:Fusionable) => (guerrero sumaAInventario (aliado.inventario)
                                                        variarEnergiaMaxima (aliado.energiaMaxima+)
                                                        aumentaEnergia (aliado.energia)
                                                        transformateEn (Fusionado(guerrero, aliado)))
         case _ => guerrero
       }   
-    }}
+    }
+  }
    
-    case class Magia(paseDeMagia: Combatientes => Combatientes) extends Movimiento {
+  case class Magia(paseDeMagia: Combatientes => Combatientes) extends Movimiento {
     def movimiento = combatientes => {
       val(atacante, oponente): Combatientes = combatientes
+
       atacante.especie match {
         case _:Magico => paseDeMagia(combatientes)
         case _ if (atacante tiene (EsferaDelDragon, 7)) =>
                                 paseDeMagia (atacante gastarItems (List.fill(7)(EsferaDelDragon)), oponente)
         case _ => combatientes
       }
-    
     }
   }
   
-    
   type Daños = (Int, Int)
   
   trait TipoAtaque
@@ -167,18 +156,13 @@ object Simulador {
         case (_, Energia, Androide) => guerrero.aumentaEnergia _
         case _ => guerrero.disminuiEnergia _
       }
-      
-  
-      
+
       combatientes.onEach( _ disminuiEnergia dañoAtacante, efectoEnAtacado(_)(dañoAtacado) )
     }
-  
   }
 
   case object MuchosGolpesNinja extends Ataque(Fisico){
-    
     def funcionDaño = { case (atacante, oponente) => {
-    
          (atacante.especie, oponente.especie) match {
            case (Humano, Androide) => (10, 0)
            case _ if atacante.energia < oponente.energia => (20, 0)
@@ -189,10 +173,7 @@ object Simulador {
   }
   
   case object Explotar extends Ataque(Fisico){
-    
     def funcionDaño = { case (atacante, oponente) => {
-  
-    
         val energiaDeExplosion = atacante.energia
         
         def factorExplosivo: Int = atacante.especie match {
@@ -211,37 +192,28 @@ object Simulador {
         }
         
         daño onSnd esquivaLaMuerte
-        
       }
     }
-    
   }
   
   case class Onda(energiaNecesaria: Int) extends Ataque(Energia){
-  
-  def funcionDaño = { case (atacante, oponente) => {
-    
+    def funcionDaño = { case (atacante, oponente) => {
         def poderDeOnda = oponente.especie match {
-                         case Monstruo(_) => energiaNecesaria / 2
-                         case _ => energiaNecesaria * 2 }
+          case Monstruo(_) => energiaNecesaria / 2
+          case _ => energiaNecesaria * 2 
+        }
      
         if (atacante.energia > energiaNecesaria) (energiaNecesaria, poderDeOnda)
         else (0, 0)
-      
       } 
     }
-    
+  }
   
-}
-  
-  case object Genkidama extends Ataque(Energia){
-    
-    def funcionDaño = { case(atacante, _) =>
-     
+  case object Genkidama extends Ataque(Energia) {
+    def funcionDaño = { case (atacante, _) =>
       val poderAcumulado = 10 pow atacante.turnosFajado
       
       (0, poderAcumulado)
-      
     }  
   }
 }
